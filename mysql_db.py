515793c59598396e6c517ac4f3a4cc6390a1d2b7
PhD. Diaconescu Tiberiu <89382386+sant3e@users.noreply.github.com>
2022-06-07 11:45:07 +0300

    update

--- mysql_db.py
+++ mysql_db.py
@@ -1,15 +1,18 @@
 # mysql db needs a server (install MySQL Community Server)
-# creating the db and tables can be done via a Python application (like we did in the sqlite_db.py)
-# or using MySQL's GUi (MySQL Workbench) or even in a terminal
+# creating the db and tables can be done via:
+# Python application (like we did in the sqlite_db.py) or
+# using MySQL's GUi (MySQL Workbench) or 
+# in a terminal (like CMD in Windows)
 
-# to connect in Python, we need pip install mysql-connector (do this in an venv)
+# to connect in Python, we need pip install mysql-connector-python (do this in an venv)
 
+# this code just connects to an already existant db (assume we created it in GUi which is easier)
 import mysql.connector as mysql
 
 def connect(db_name):
 	try:
 		return mysql.connect(
-			host='localhost',
+			host='localhost or server address',
 			user='root',
 			password='password',
 			database=db_name)
@@ -17,11 +20,13 @@ def connect(db_name):
 		print(e)
 
 if __name__ == '__main__':
-	db = connect("projects")
+	db = connect("mydbname")
 	cursor = db.cursor()
-
-	cursor.execute("SELECT * FROM projects")
+	db.close()
+	
+	# Checking the records
+	cursor.execute("SELECT * FROM mytablename")
 	project_records = cursor.fetchall()
 	print(project_records)
 
-	db.close()
+	
