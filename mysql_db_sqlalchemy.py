e2b89436ffa1f778fcd95f2dbcd5e28d197c4fac
PhD. Diaconescu Tiberiu <89382386+sant3e@users.noreply.github.com>
2022-06-07 15:18:57 +0300

    Update code

--- mysql_db_sqlalchemy.py
+++ mysql_db_sqlalchemy.py
@@ -38,3 +38,27 @@ class Mytable2name(Base):
 		return "<Mytable2name(col3='{0}, ...)'>".format(self.col3, ...)
 
 Base.metadata.create_all(engine)
+
+----------
+## to query the db we need a sessionmaker
+session_maker = sessionmaker()
+session_maker.configure(bind=engine)
+session = session_maker()
+
+# inserting values; use the Class defined above
+value_for_table1 = Mytable1name(col2='Value', col3='Value', ...)
+session.add(value_for_table1)    # if you add only one value
+session.commit()
+
+value_for_table2 = [Mytable2name(col_id_specific_to_table1=value_for_table1.col_id_specific_to_table1, col3='Value1'),
+Mytable2name(col_id_specific_to_table1=value_for_table1.col_id_specific_to_table1, col3='Value2'),
+Mytable2name(col_id_specific_to_table1=value_for_table1.col_id_specific_to_table1, col3='Value3'),...]
+session.bulk_save_objects(value_for_table2)    # if you add multiple values
+session.commit()
+
+# filter records
+query_table1 = session.query(Mytable1name).filter_by(col2='Value').first()    #use first if you want only 1 result
+print(query_table1)
+
+query_table2 = session.query(Mytable2name).all()    #use all if you want to retrieve all results
+print(query_table2)
